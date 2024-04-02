9e870ac943fed97201e277545a8dd39e7f4018a1
Priyanka <priyankareddy0910@gmail.com>
2024-04-02 21:46:08 +0530

    testing done

---
+++ src/test/java/Priyankaacademy/SeleniumFrameworkDesign/Priyanka.java
@@ -0,0 +1,15 @@
+package Priyankaacademy.SeleniumFrameworkDesign;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Priyanka {
+	
+	
+	@Test
+	public void m1() {
+		
+		System.out.println("hey");
+	}
+
+}
